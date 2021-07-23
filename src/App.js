b8fcfd0c992342264ff6296e4ebc2069057d57c0
kevinlutz <kevinlutz@gmail.com>
2021-07-23 15:05:31 -0400

    created nav hero and coursecard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PrimarySearchAppBar from "./Nav";
+import CourseCard from "./CourseCard";
+import Hero from "./Hero";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PrimarySearchAppBar />
+      <Hero />
+      <CourseCard />
     </div>
   );
 }
---
+++ src/Hero.js
@@ -0,0 +1,11 @@
+import Container from "./Container";
+import React from "react";
+
+export default function Hero() {
+  return (
+    <Container>
+      <h1>hello</h1>
+      <img src="/images/randomdude.jpg" alt="random dude" />
+    </Container>
+  );
+}
